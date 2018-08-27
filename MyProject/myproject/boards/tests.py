38ac01b66de12371c3a7d07ee967ae51d982af6e
DELL <sadashivgomadi@yahoo.in>
2018-08-27 19:08:52 +0530

    Testing the modules and the status codes.

--- MyProject/myproject/boards/tests.py
+++ MyProject/myproject/boards/tests.py
@@ -4,17 +4,37 @@ from django.test import TestCase
 
 from django.urls import reverse
 from django.urls import resolve
-from .views import home
+from .views import home, board_topics
+from .models import Board
+
 
 class HomeTests(TestCase):
     def test_home_view_status_code(self):
         url = reverse('home')
         response = self.client.get(url)
-        self.assertEquals(response.status_code, 200) #check if the status code matches with 200, else throw an exception
+        self.assertEquals(response.status_code, 200)  #check if the status code matches with 200, else throw an exception
 
-#we can test if Django returned the correct view function for the requested URL with the below function
+    #we can test if Django returned the correct view function for the requested URL with the below function
     def test_home_url_resolves_home_view(self):
         view = resolve('/')  # '/' is the root url.
         self.assertEquals(view.func, home)
 '''This test will make sure the URL / which is the root URL, is returning the home view'''
 
+
+class BoardTopicsTests(TestCase):
+    def setUp(self):
+        Board.objects.create(name = 'Django', description = 'Django board.')
+
+    def test_board_topics_view_success_status_code(self):
+        url = reverse('board_topics',kwargs={'pk' : 1})
+        response = self.client.get(url)
+        self.assertEquals(response.status_code, 200)
+
+    def test_board_topics_view_not_found_status_code(self):
+        url = reverse('board_topics', kwargs={'pk': 99})
+        response = self.client.get(url)
+        self.assertEquals(response.status_code, 404)
+
+    def test_board_topics_url_resolves_board_topics_view(self):
+        view = resolve('/boards/2/')
+        self.assertEquals(view.func, board_topics)
--- MyProject/myproject/boards/views.py
+++ MyProject/myproject/boards/views.py
@@ -2,12 +2,15 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
+#from django.http import Http404
 from .models import Board
+from django.shortcuts import render, get_object_or_404
 
 def home(request):
 	boards = Board.objects.all()
 	return render(request, 'home.html', {'boards': boards})
 
+
 def board_topics(request, pk):
-	board = Board.objects.get(pk=pk)
+	board = get_object_or_404(Board, pk=pk)
 	return render(request, 'topics.html', {'board':board})
