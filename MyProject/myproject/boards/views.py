e5ad7bf637bfc2c621110fca82e717e160d84d9c
DELL <sadashivgomadi@yahoo.in>
2018-08-28 21:55:38 +0530

    Adding new_topic.html

--- MyProject/myproject/boards/views.py
+++ MyProject/myproject/boards/views.py
@@ -7,10 +7,15 @@ from .models import Board
 from django.shortcuts import render, get_object_or_404
 
 def home(request):
-	boards = Board.objects.all()
-	return render(request, 'home.html', {'boards': boards})
+    boards = Board.objects.all()
+    return render(request, 'home.html', {'boards': boards})
 
 
 def board_topics(request, pk):
-	board = get_object_or_404(Board, pk=pk)
-	return render(request, 'topics.html', {'board':board})
+    board = get_object_or_404(Board, pk=pk)
+    return render(request, 'topics.html', {'board':board})
+
+
+def new_topic(request,pk):
+    board = get_object_or_404(Board, pk = pk)
+    return render(request, 'new_topic.html', {'board': board})
--- MyProject/myproject/myproject/urls.py
+++ MyProject/myproject/myproject/urls.py
@@ -25,6 +25,8 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^$', views.home, name = 'home'),
     url(r'^boards/(?P<pk>\d+)/$', views.board_topics, name = 'board_topics'),
+    url(r'^boards/(?P<pk>\d+)/new/$', views.new_topic, name='new_topic'),
     path('admin/', admin.site.urls),
 	#url(r'^admin/',admin.site.urls),  #if this is uncommented it throws a warning
+
 ]
