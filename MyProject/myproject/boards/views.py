f04eca0e763dd19a41763d89aa1072334e44c092
DELL <sadashivgomadi@yahoo.in>
2018-08-29 19:41:52 +0530

    new_topic view is now able to take the data entered by the user, however no validation has been done on the data yet.

--- MyProject/myproject/boards/views.py
+++ MyProject/myproject/boards/views.py
@@ -1,10 +1,9 @@
-from django.shortcuts import render
-
 # Create your views here.
 from django.http import HttpResponse
 #from django.http import Http404
-from .models import Board
-from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.models import User
+from .models import Board,Topic, Post
+from django.shortcuts import render, get_object_or_404, redirect
 
 def home(request):
     boards = Board.objects.all()
@@ -18,4 +17,25 @@ def board_topics(request, pk):
 
 def new_topic(request,pk):
     board = get_object_or_404(Board, pk = pk)
+
+    if request.method == 'POST':
+        subject = request.POST['subject']
+        message = request.POST['message']
+
+        user = User.objects.first()  #TODO: get the currently logged in as the user
+
+        topic = Topic.objects.create(
+            subject = subject,
+            board = board,
+            starter = user
+        )
+
+        post = Post.objects.create(
+            message = message,
+            topic = topic,
+            created_by = user
+        )
+
+        return redirect('board_topics', pk = board.pk)  # TODO: redirect to the created topic page
+
     return render(request, 'new_topic.html', {'board': board})
