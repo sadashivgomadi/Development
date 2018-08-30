4d573a5ff83afd2046c16466ea4c893b7079f0a8
DELL <sadashivgomadi@yahoo.in>
2018-08-30 20:20:20 +0530

    Changes to the code when the csrf test function was returning a none object instead of HttpResponse.

--- MyProject/myproject/boards/views.py
+++ MyProject/myproject/boards/views.py
@@ -1,8 +1,6 @@
 # Create your views here.
-from django.http import HttpResponse
-#from django.http import Http404
 from django.contrib.auth.models import User
-from .models import Board,Topic, Post
+from .models import Board, Topic , Post
 from django.shortcuts import render, get_object_or_404, redirect
 from .forms import NewTopicForm
 
@@ -16,22 +14,22 @@ def board_topics(request, pk):
     return render(request, 'topics.html', {'board':board})
 
 
-def new_topic(request,pk):
-    board = get_object_or_404(Board, pk = pk)
+def new_topic(request, pk):
+    board = get_object_or_404(Board, pk=pk)
     user = User.objects.first()  # TODO: get the currently logged in as the user
     if request.method == 'POST':
         form = NewTopicForm(request.POST)
         if form.is_valid():
-            topic = form.save(commit = False)
+            topic = form.save(commit=False)
             topic.board = board
             topic.starter = user
             topic.save()
             post = Post.objects.create(
-                message = message,
-                topic = topic,
-                created_by = user
+                message=form.cleaned_data.get('message'),
+                topic=topic,
+                created_by=user
             )
-            return redirect('board_topics', pk = board.pk)  # TODO: redirect to the created topic page
-        else:
-            form = NewTopicForm()
-    return render(request, 'new_topic.html', {'board': board, 'form':form})
+            return redirect('board_topics', pk=board.pk)  # TODO: redirect to the created topic page
+    else:
+        form = NewTopicForm()
+    return render(request, 'new_topic.html', {'board': board, 'form': form})
