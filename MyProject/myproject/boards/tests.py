74155e41e491b3bdaf833263b4a7cec8dd604f6e
DELL <sadashivgomadi@yahoo.in>
2018-08-30 16:22:57 +0530

    Adding NewTopic testcases

--- MyProject/myproject/boards/tests.py
+++ MyProject/myproject/boards/tests.py
@@ -5,7 +5,8 @@ from django.test import TestCase
 from django.urls import reverse
 from django.urls import resolve
 from .views import home, board_topics, new_topic
-from .models import Board
+from .models import Board, Topic, Post
+from django.contrib.auth.models import User
 
 
 class HomeTests(TestCase):
@@ -51,6 +52,18 @@ class BoardTopicsTests(TestCase):
     def test_board_topics_url_resolves_board_topics_view(self):
         view = resolve('/boards/2/')
         self.assertEquals(view.func, board_topics)
+
+    def test_board_topics_view_contains_navigation_links(self):
+        board_topics_url = reverse('board_topics', kwargs={'pk': 1})
+        homepage_url = reverse('home')
+        new_topic_url = reverse('new_topic', kwargs={'pk':1})
+
+        response = self.client.get(board_topics_url)
+
+        self.assertContains(response, 'href="{0}"'.format(homepage_url))
+        self.assertContains(response, 'href="{0}"'.format(new_topic_url))
+
+
 #Tests for New Topic Page
 class NewTopicTests(TestCase):
     def setUp(self):
@@ -75,3 +88,49 @@ class NewTopicTests(TestCase):
         board_topics_url = reverse('board_topics', kwargs={'pk':1})
         response = self.client.get(new_topic_url)
         self.assertContains(response, 'href="{0}"'.format(board_topics_url))
+
+class NewTopicTests(TestCase):
+    def setUp(self):
+        Board.objects.create(name='Django',description='Django Board')
+        User.objects.create_user(username='john', email='john@doe.com',password='123')
+
+
+    def test_csrf(self):
+        url = reverse('new_topic',kwargs={'pk':1})
+        response = self.client.get(url)
+        self.assertContains(response, 'csrfmiddlewaretoken')
+
+    def test_new_topic_valid_post_data(self):
+        url = reverse('new_topic', kwargs={'pk':1})
+        data = {
+            'subject' : 'Test title',
+            'message' : 'Lorem ipsum dolor sit amet'
+        }
+        response = self.client.post(url,data)
+        self.assertTrue(Topic.objects.exists())
+        self.assertTrue(Post.objects.exists())
+
+    def test_new_topic_invalid_post_data(self):
+        '''
+        Invalid post data should not redirect
+        The expected behaviour is to show the form again with validation errors
+        '''
+        url = reverse('new_topic', kwargs={'pk':1})
+        response = self.client.post(url, {})
+        self.assertEquals(response.status_code, 200)
+
+    def test_new_topic_invalid_post_data_empty_fields(self):
+        '''
+        Invalid post data should not redirect
+        The expected behaviour is to show the form again with validation errors
+        '''
+        url = reverse('new_topic', kwargs={'pk': 1})
+        data = {
+            'subject' : '',
+            'message' : ''
+         }
+        response = self.client.post(url,data)
+        self.assertEquals(response.status_code, 200)
+        self.assertFalse(Topic.objects.exists())
+        self.assertFalse(Post.objects.exists())
+
