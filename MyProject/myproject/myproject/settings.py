f19e3433d30e4d25c398d23cd3141ff88a2d41a1
DELL <sadashivgomadi@yahoo.in>
2018-08-30 20:33:08 +0530

    adding django-widget-tweaks

--- MyProject/myproject/myproject/settings.py
+++ MyProject/myproject/myproject/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-	'boards',
+    'widget_tweaks',
+    'boards',
 ]
 
 MIDDLEWARE = [
@@ -120,4 +121,4 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR,'static')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
